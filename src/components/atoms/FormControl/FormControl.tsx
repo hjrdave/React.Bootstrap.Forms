075e5d0a235f2cfc8cec8f81538d7578fe68f9e2
David Sanders <dsanders_m633@outlook.com>
2023-02-23 11:10:20 -0500

    created FormControl comp and interface

--- src/components/atoms/FormControl/FormControl.tsx
+++ src/components/atoms/FormControl/FormControl.tsx
@@ -1,38 +1,8 @@
 import React from 'react';
-import { Form, InputGroup } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
+import FormControlProps from './FormControlProps';
 
-interface Props {
-    name: string;
-    label?: string | JSX.Element;
-    labelPosition?: 'top' | 'column';
-    className?: string;
-    placeholder?: string;
-    type?: string;
-    ref?: any;
-    id?: string;
-    as?: 'input' | 'textarea' | React.ElementType;
-    disabled?: boolean;
-    htmlSize?: number;
-    isInvalid?: boolean;
-    isValid?: boolean;
-    onChange?: (e: any) => void;
-    plaintext?: boolean;
-    readOnly?: boolean;
-    value?: string | any[] | number;
-    controlId: string;
-    htmlFor?: string;
-    bsPrefix?: string;
-    size?: 'sm' | 'lg';
-    ariaDescribedby?: string;
-    ariaLabel?: string;
-    caption?: string;
-    captionId?: string;
-    hasValidation?: boolean;
-    appendControl?: (props: { [key: string]: any }) => JSX.Element;
-    prependControl?: (props: { [key: string]: any }) => JSX.Element;
-    tabIndex?: number;
-};
-export default function FormControl({ className, controlId, label, htmlFor, ariaDescribedby, caption, captionId, appendControl: AppendControl, prependControl: PrependControl, ariaLabel, ...props }: Props) {
+export default function FormControl({ className, controlId, label, htmlFor, ariaDescribedby, caption, captionId, appendControl: AppendControl, prependControl: PrependControl, ariaLabel, ...props }: FormControlProps) {
 
     return (
         <>
---
+++ src/components/atoms/FormControl/FormControlProps.ts
@@ -0,0 +1,31 @@
+export default interface FormControlProps {
+    name: string;
+    label?: string | JSX.Element;
+    labelPosition?: 'top' | 'column';
+    className?: string;
+    placeholder?: string;
+    type?: string;
+    ref?: any;
+    id?: string;
+    as?: 'input' | 'textarea' | React.ElementType;
+    disabled?: boolean;
+    htmlSize?: number;
+    isInvalid?: boolean;
+    isValid?: boolean;
+    onChange?: (e: any) => void;
+    plaintext?: boolean;
+    readOnly?: boolean;
+    value?: string | any[] | number;
+    controlId: string;
+    htmlFor?: string;
+    bsPrefix?: string;
+    size?: 'sm' | 'lg';
+    ariaDescribedby?: string;
+    ariaLabel?: string;
+    caption?: string;
+    captionId?: string;
+    hasValidation?: boolean;
+    appendControl?: (props: { [key: string]: any }) => JSX.Element;
+    prependControl?: (props: { [key: string]: any }) => JSX.Element;
+    tabIndex?: number;
+}
